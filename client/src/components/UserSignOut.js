2a0348f096832ba3b6ec3166187bdc11cf16c59f
GabbyJJ <76443093+GabbyJJ@users.noreply.github.com>
2021-06-04 14:20:21 -0500

    Final Committ 6-4

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 
+//This will sign out the user and redirect them to the home screen
+
 const UserSignOut = () => {
   localStorage.setItem("user", null);
   return <Redirect to={"/"} />;
